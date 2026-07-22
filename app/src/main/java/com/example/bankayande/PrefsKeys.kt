9b2445a7b1ad038e4d5f574eb014dc3e4928a33d
mamadya <yas08yas@gmail.com>
2026-07-22 22:20:08 +0330

    Create PrefsKeys.kt

---
+++ app/src/main/java/com/example/bankayande/PrefsKeys.kt
@@ -0,0 +1,8 @@
+package com.example.bankayande
+
+object PrefsKeys {
+    const val FILE = "bank_ayande_prefs"
+    const val PASSWORD = "password"
+    const val BALANCE = "balance"
+    const val SETUP_DONE = "setup_done"
+}
